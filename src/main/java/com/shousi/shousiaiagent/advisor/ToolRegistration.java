2dd21dbccd001b02a1d8fec9a39b8d9a1161e830
shousi18 <1416239139@qq.com>
2025-05-22 10:55:15 +0800

    feat:工具集中注册以及使用工具;

---
+++ src/main/java/com/shousi/shousiaiagent/advisor/ToolRegistration.java
@@ -0,0 +1,33 @@
+package com.shousi.shousiaiagent.advisor;
+
+import com.shousi.shousiaiagent.tools.*;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbacks;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ToolRegistration {
+
+    @Value("${searchapi.api-key}")
+    private String searchApiKey;
+
+    @Bean
+    public ToolCallback[] allTools() {
+        FileOperationTool fileOperationTool = new FileOperationTool();
+        PDFGenerationTool pdfGenerationTool = new PDFGenerationTool();
+        WebSearchTool webSearchTool = new WebSearchTool(searchApiKey);
+        ResourceDownloadTool resourceDownloadTool = new ResourceDownloadTool();
+        WebScrapingTool webScrapingTool = new WebScrapingTool();
+        WeatherTools weatherTools = new WeatherTools();
+        return ToolCallbacks.from(
+                fileOperationTool,
+                pdfGenerationTool,
+                webSearchTool,
+                resourceDownloadTool,
+                webScrapingTool,
+                weatherTools
+        );
+    }
+}
